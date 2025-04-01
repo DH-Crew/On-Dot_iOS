5141ab92effb19b6adbf960a004eba4aeb327bde
dogmania <hj296914@gmail.com>
2025-04-02 01:51:45 +0900

    :sparkles: Feat: 프로젝트 생성

---
+++ On-Dot/On-Dot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 4/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ On-Dot/On-DotTests/On_DotTests.swift
@@ -0,0 +1,17 @@
+//
+//  On_DotTests.swift
+//  On-DotTests
+//
+//  Created by 현수 노트북 on 4/2/25.
+//
+
+import Testing
+@testable import On_Dot
+
+struct On_DotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
