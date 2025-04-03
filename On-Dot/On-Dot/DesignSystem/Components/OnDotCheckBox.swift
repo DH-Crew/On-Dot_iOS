ee0c7fe8670bad32c87fec127f48799f3d0bf88e
dogmania <hj296914@gmail.com>
2025-04-04 03:41:30 +0900

    :recycle: Refactor: Preview 테스트 유틸 적용

--- On-Dot/On-Dot/DesignSystem/Components/OnDotCheckBox.swift
+++ On-Dot/On-Dot/DesignSystem/Components/OnDotCheckBox.swift
@@ -22,23 +22,3 @@ struct OnDotCheckBox: View {
             }
     }
 }
-
-struct StatefulPreviewWrapper<Value>: View {
-    @State var value: Value
-    var content: (Binding<Value>) -> AnyView
-
-    init(_ value: Value, @ViewBuilder content: @escaping (Binding<Value>) -> some View) {
-        self._value = State(wrappedValue: value)
-        self.content = { binding in AnyView(content(binding)) }
-    }
-
-    var body: some View {
-        content($value)
-    }
-}
-
-#Preview {
-    StatefulPreviewWrapper(false) { isChecked in
-        OnDotCheckBox(isChecked: isChecked, size: 40, action: {})
-    }
-}
