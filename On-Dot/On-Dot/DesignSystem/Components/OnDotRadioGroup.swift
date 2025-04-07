91d69853cb7cd87cf3271fd9d6eb7762f1993de1
dogmania <hj296914@gmail.com>
2025-04-08 00:43:12 +0900

    :sparkles: Feat: 공통 라디오 그룹 구현

---
+++ On-Dot/On-Dot/DesignSystem/Components/OnDotRadioGroup.swift
@@ -0,0 +1,38 @@
+//
+//  OnDotRadioGroup.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 4/8/25.
+//
+
+import SwiftUI
+
+struct OnDotRadioGroup<T: Hashable & Identifiable>: View {
+    var items: [T]
+    var label: (T) -> String
+    @Binding var selected: T
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ForEach(items) { item in
+                HStack(spacing: 8) {
+                    Circle()
+                        .fill(selected == item ? Color.green800 : Color.gray400)
+                        .frame(width: 16, height: 16)
+                        .overlay(
+                            Circle()
+                                .fill(Color.green500)
+                                .frame(width: selected == item ? 10 : 0, height: selected == item ? 10 : 0)
+                        )
+                    
+                    Text(label(item))
+                        .font(OnDotTypo.bodyMediumR)
+                        .foregroundColor(.gray0)
+                }
+                .onTapGesture {
+                    selected = item
+                }
+            }
+        }
+    }
+}
