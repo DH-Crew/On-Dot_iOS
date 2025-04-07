82d9284da689078121a72904da70e48162a0f99b
dogmania <hj296914@gmail.com>
2025-04-08 02:11:28 +0900

    :sparkles: Feat: 공통 컴포넌트 구현

--- On-Dot/On-Dot/ContentView.swift
+++ On-Dot/On-Dot/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isOn: Bool = false
+    @State var isOnTrue: Bool = true
+    @State var isChecked: Bool = false
+    @State var isCheckedTrue: Bool = true
+    @State var selectedInterval: AlarmInterval = .one
+    @State var showDialog: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.gray900
+                .ignoresSafeArea()
+            
+            VStack(alignment: .center) {
+                HStack(spacing: 10) {
+                    OnDotButton(content: "버튼", action: {})
+                }
+                
+                HStack(spacing: 10) {
+                    OnDotToggle(isOn: $isOn)
+                    OnDotToggle(isOn: $isOnTrue)
+                }
+                
+                HStack(spacing: 10) {
+                    OnDotCheckBox(isChecked: $isChecked)
+                    OnDotCheckBox(isChecked: $isCheckedTrue)
+                }
+                
+                HStack(spacing: 10) {
+                    TextChip(title: "라벨", isActive: true)
+                    TextChip(title: "라벨", isActive: false)
+                }
+                
+                HStack(spacing: 10) {
+                    CheckTextChip(title: "라벨", style: CheckTextChip.Style.active)
+                    CheckTextChip(title: "라벨", style: CheckTextChip.Style.inactive)
+                    CheckTextChip(title: "라벨", style: CheckTextChip.Style.normal)
+                }
+                
+                HStack(spacing: 10) {
+                    WeekDayChip(title: "일", style: WeekDayChip.Style.active)
+                    WeekDayChip(title: "일", style: WeekDayChip.Style.inactive)
+                    WeekDayChip(title: "일", style: WeekDayChip.Style.normal)
+                }
+                
+                HStack {
+                    OnDotRadioGroup(items: AlarmInterval.allCases, label: { $0.label }, selected: $selectedInterval)
+                }
+                
+                HStack {
+                    CreateScheduleMenu(onClickQuickSchedule: {}, onClickGeneralSchedule: {})
+                }
+            }
+            .padding()
+            
+            if showDialog {
+                OnDotDialog(
+                    title: "변경 사항 삭제", content: "변경 사항을 저장하지 않고\n나가시겠어요?",
+                    positiveButtonText: "나가기", negativeButtonText: "취소",
+                    onClickBtnPositive: {}, onClickBtnNegative: {}, onDismissRequest: {}
+                )
+            }
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
