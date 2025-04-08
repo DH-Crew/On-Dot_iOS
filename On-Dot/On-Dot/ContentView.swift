9a772184de4aabadc12d963b7810a7cde6cafaa2
dogmania <hj296914@gmail.com>
2025-04-09 02:51:40 +0900

    :sparkles: Feat: FromToLocationView 구현

--- On-Dot/On-Dot/ContentView.swift
+++ On-Dot/On-Dot/ContentView.swift
@@ -8,12 +8,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var isOn: Bool = false
-    @State var isOnTrue: Bool = true
-    @State var isChecked: Bool = false
-    @State var isCheckedTrue: Bool = true
-    @State var selectedInterval: AlarmInterval = .one
-    @State var showDialog: Bool = false
     
     var body: some View {
         ZStack {
@@ -21,54 +15,9 @@ struct ContentView: View {
                 .ignoresSafeArea()
             
             VStack(alignment: .center) {
-                HStack(spacing: 10) {
-                    OnDotButton(content: "버튼", action: {})
-                }
-                
-                HStack(spacing: 10) {
-                    OnDotToggle(isOn: $isOn)
-                    OnDotToggle(isOn: $isOnTrue)
-                }
-                
-                HStack(spacing: 10) {
-                    OnDotCheckBox(isChecked: $isChecked)
-                    OnDotCheckBox(isChecked: $isCheckedTrue)
-                }
-                
-                HStack(spacing: 10) {
-                    TextChip(title: "라벨", isActive: true)
-                    TextChip(title: "라벨", isActive: false)
-                }
-                
-                HStack(spacing: 10) {
-                    CheckTextChip(title: "라벨", style: CheckTextChip.Style.active)
-                    CheckTextChip(title: "라벨", style: CheckTextChip.Style.inactive)
-                    CheckTextChip(title: "라벨", style: CheckTextChip.Style.normal)
-                }
-                
-                HStack(spacing: 10) {
-                    WeekDayChip(title: "일", style: WeekDayChip.Style.active)
-                    WeekDayChip(title: "일", style: WeekDayChip.Style.inactive)
-                    WeekDayChip(title: "일", style: WeekDayChip.Style.normal)
-                }
-                
-                HStack {
-                    OnDotRadioGroup(items: AlarmInterval.allCases, label: { $0.label }, selected: $selectedInterval)
-                }
-                
-                HStack {
-                    CreateScheduleMenu(onClickQuickSchedule: {}, onClickGeneralSchedule: {})
-                }
+                FromToLocationView()
             }
             .padding()
-            
-            if showDialog {
-                OnDotDialog(
-                    title: "변경 사항 삭제", content: "변경 사항을 저장하지 않고\n나가시겠어요?",
-                    positiveButtonText: "나가기", negativeButtonText: "취소",
-                    onClickBtnPositive: {}, onClickBtnNegative: {}, onDismissRequest: {}
-                )
-            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
---
+++ On-Dot/On-Dot/DesignSystem/Components/Composite/FromToLocationView.swift
@@ -0,0 +1,76 @@
+//
+//  FromToLocationView.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 4/9/25.
+//
+
+import SwiftUI
+
+struct FromToLocationView: View {
+    @State var fromLocation: String = "서울 드림헌터로 성공하길 17"
+    @State var toLocation: String = ""
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            Spacer().frame(width: 10)
+            
+            Image("ic_swap_vertical")
+                .onTapGesture { swapLocation() }
+            
+            Spacer().frame(width: 10)
+            
+            VStack(alignment: .leading, spacing: 0) {
+                locationView(image: "ic_from_location", title: "출발지", content: fromLocation)
+                Spacer().frame(height: 16)
+                Spacer()
+                    .frame(maxWidth: .infinity, maxHeight: 0.5)
+                    .background(Color.gray600)
+                Spacer().frame(height: 16)
+                locationView(image: "ic_to_location", title: "도착지", content: toLocation)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.vertical, 16)
+        .background(Color.gray700)
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+    }
+    
+    @ViewBuilder
+    private func locationView(
+        image: String,
+        title: String,
+        content: String
+    ) -> some View {
+        HStack(alignment: .center, spacing: 0) {
+            Image(image)
+            Spacer().frame(width: 8)
+            Text("\(title): ")
+                .font(OnDotTypo.bodyLargeR1)
+                .foregroundStyle(content.isEmpty ? Color.gray300 : Color.gray0)
+            if content.isEmpty {
+                Text("입력")
+                    .font(OnDotTypo.bodyLargeR1)
+                    .foregroundStyle(Color.gray300)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            } else {
+                Text(content)
+                    .font(OnDotTypo.bodyLargeR1)
+                    .foregroundStyle(Color.gray0)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            Spacer().frame(width: 8)
+            if !content.isEmpty {
+                Image("ic_close")
+                Spacer().frame(width: 20)
+            }
+        }
+        .frame(maxWidth: .infinity)
+    }
+    
+    private func swapLocation() {
+        let temp = fromLocation
+        fromLocation = toLocation
+        toLocation = temp
+    }
+}
