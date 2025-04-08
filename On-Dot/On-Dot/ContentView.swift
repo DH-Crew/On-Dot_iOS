e39ac823ecac8e73b7ae4b413caa18fa1b299f61
dogmania <hj296914@gmail.com>
2025-04-09 03:08:19 +0900

    :safety_vest: Feat: SelectedTimeView rngus

--- On-Dot/On-Dot/ContentView.swift
+++ On-Dot/On-Dot/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .ignoresSafeArea()
             
             VStack(alignment: .center) {
-                SelectedDateView()
+                SelectedTimeView()
             }
             .padding()
         }
---
+++ On-Dot/On-Dot/DesignSystem/Components/Composite/SelectedTimeView.swift
@@ -0,0 +1,25 @@
+//
+//  SelectedTimeView.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 4/9/25.
+//
+
+import SwiftUI
+
+struct SelectedTimeView: View {
+    @State var selectedTime: String = "오후 7:00"
+    
+    var body: some View {
+        HStack {
+            Text("시간")
+                .font(OnDotTypo.bodyLargeR1)
+                .foregroundStyle(Color.gray0)
+            
+            Spacer()
+            
+            TextChip(title: selectedTime, isActive: true)
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
