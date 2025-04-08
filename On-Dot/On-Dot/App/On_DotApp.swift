bba566f8fe316fe958a548ceba8e1f98c72b9be5
dogmania <hj296914@gmail.com>
2025-04-08 23:35:18 +0900

    :recycle: Refactor: 폴더 구조 변경

---
+++ On-Dot/On-Dot/App/On_DotApp.swift
@@ -0,0 +1,17 @@
+//
+//  On_DotApp.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct On_DotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ On-Dot/On-Dot/View/Main/MainView.swift
@@ -0,0 +1,14 @@
+//
+//  MainView.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 4/8/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        
+    }
+}
