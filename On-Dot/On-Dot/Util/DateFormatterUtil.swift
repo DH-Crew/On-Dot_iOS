588af4ab9e75e4b4e805d36f9306037c810bd3c0
dogmania <hj296914@gmail.com>
2025-04-10 01:29:52 +0900

    :recycle: Refactor: 구조체명 변경

--- On-Dot/On-Dot/Util/DateFormatterUtil.swift
+++ On-Dot/On-Dot/Util/DateFormatterUtil.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct DateFormatter {
+struct DateFormatterUtil {
     
     /// 날짜를 "yyyy.MM.dd" 형식으로 반환
     static func formatDate(_ date: Date) -> String {
@@ -20,7 +20,7 @@ struct DateFormatter {
     /// 시간을 "오전/오후 h:mm" 형식으로 반환
     static func formatTime(_ date: Date) -> String {
         let formatter = DateFormatter()
-        formatter.dateFormat = "a h:mm"
+        formatter.dateFormat = "a hh:mm"
         formatter.locale = Locale(identifier: "ko_KR")
         formatter.amSymbol = "오전"
         formatter.pmSymbol = "오후"
@@ -30,7 +30,7 @@ struct DateFormatter {
     /// 날짜 + 시간 형식으로 반환 (예: "2025.04.08 오전 9:00")
     static func formatDateTime(_ date: Date) -> String {
         let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy.MM.dd a h:mm"
+        formatter.dateFormat = "yyyy.MM.dd a hh:mm"
         formatter.locale = Locale(identifier: "ko_KR")
         formatter.amSymbol = "오전"
         formatter.pmSymbol = "오후"
