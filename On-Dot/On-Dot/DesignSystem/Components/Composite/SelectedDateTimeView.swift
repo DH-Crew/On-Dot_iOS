00e0a896eafd09fa7598299970f3e0edbaaf4c8c
dogmania <hj296914@gmail.com>
2025-04-09 23:01:54 +0900

    :recycle: Refactor: 불필요한 State 제거

--- On-Dot/On-Dot/DesignSystem/Components/Composite/SelectedDateTimeView.swift
+++ On-Dot/On-Dot/DesignSystem/Components/Composite/SelectedDateTimeView.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 struct SelectedDateTimeView: View {
-    @State var selectedDate: String = "2025년 6월 13일"
-    @State var selectedTime: String = "오후 7:00"
+    var selectedDate: String = "2025년 6월 13일"
+    var selectedTime: String = "오후 7:00"
     
     var onClickDateChip: () -> Void
     var onClickTimeChip: () -> Void
