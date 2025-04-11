033908e9f9f41a7b23ff8d26c1c3919d1a104b21
dogmania <hj296914@gmail.com>
2025-04-12 01:47:51 +0900

    :sparkles: Feat: LottieView 구현

---
+++ On-Dot/On-Dot/View/Splach/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 4/12/25.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
