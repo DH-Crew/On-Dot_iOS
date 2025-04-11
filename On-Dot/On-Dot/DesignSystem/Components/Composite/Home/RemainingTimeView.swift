b7ca3e8efba1cfd17964229376585065df62d6ec
dogmania <hj296914@gmail.com>
2025-04-12 00:56:25 +0900

    :recycle: Refactor: 텍스트 하드 코딩 로직 수정

--- On-Dot/On-Dot/DesignSystem/Components/Composite/Home/RemainingTimeView.swift
+++ On-Dot/On-Dot/DesignSystem/Components/Composite/Home/RemainingTimeView.swift
@@ -13,8 +13,16 @@ struct RemainingTimeView: View {
     var minute: Int = 0
     
     var body: some View {
-        Text("\(day)일 \(hour)시간 \(minute)분 후에 울려요")
+        Text(formatRemainingTimeText())
             .font(OnDotTypo.titleMediumSB)
             .foregroundStyle(Color.gray0)
+    }
+    
+    private func formatRemainingTimeText() -> String {
+        if day == 0 && hour == 0 && minute == 0 {
+            return "곧 알람이 울려요"
+        }
+        
+        return "\(day)일 \(hour)시간 \(minute)분 후에 울려요"
     }
 }
