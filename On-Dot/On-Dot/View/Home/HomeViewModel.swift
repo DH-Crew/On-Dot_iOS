ed0f5c724601eae8a1100e56c6241edf6e632e85
dogmania <hj296914@gmail.com>
2025-04-13 03:57:25 +0900

    :recycle: Refactor: 하드코딩된 샘플 데이터 로직 수정

--- On-Dot/On-Dot/View/Home/HomeViewModel.swift
+++ On-Dot/On-Dot/View/Home/HomeViewModel.swift
@@ -8,15 +8,23 @@
 import SwiftUI
 
 final class HomeViewModel: ObservableObject {
-    @Published var scheduleList: [ScheduleModel] = [
-        ScheduleModel(id: 1, title: "일정1", isRepeat: false, repeatDays: [], appointmentAt: Date(), preparationTriggeredAt: Date(), departureTriggeredAt: Date(), isEnabled: true),
-        ScheduleModel(id: 2, title: "일정2", isRepeat: false, repeatDays: [], appointmentAt: Date(), preparationTriggeredAt: nil, departureTriggeredAt: Date(), isEnabled: false),
-        ScheduleModel(id: 3, title: "일정3", isRepeat: true, repeatDays: [1, 7], appointmentAt: Date(), preparationTriggeredAt: Date(), departureTriggeredAt: Date(), isEnabled: false)
-    ]
+    @Published var scheduleList: [ScheduleModel] = []
     @Published var isShrunk: Bool = false
     @Published var showDeleteCompletionToast: Bool = false
     
     private var recentlyDeleted: (item: ScheduleModel, index: Int)?
+    
+    init() {
+        loadSampleData()
+    }
+    
+    private func loadSampleData() {
+        scheduleList = [
+            ScheduleModel(id: 1, title: "일정1", isRepeat: false, repeatDays: [], appointmentAt: Date(), preparationTriggeredAt: Date(), departureTriggeredAt: Date(), isEnabled: true),
+            ScheduleModel(id: 2, title: "일정2", isRepeat: false, repeatDays: [], appointmentAt: Date(), preparationTriggeredAt: nil, departureTriggeredAt: Date(), isEnabled: false),
+            ScheduleModel(id: 3, title: "일정3", isRepeat: true, repeatDays: [1, 7], appointmentAt: Date(), preparationTriggeredAt: Date(), departureTriggeredAt: Date(), isEnabled: false)
+        ]
+    }
     
     func deleteSchedule(id: Int) {
         if let index = scheduleList.firstIndex(where: { $0.id == id }) {
