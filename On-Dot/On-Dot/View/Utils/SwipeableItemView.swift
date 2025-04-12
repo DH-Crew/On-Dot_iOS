b88ee313ea6319f0b347e577ff1e2eedb4200f15
dogmania <hj296914@gmail.com>
2025-04-13 03:36:52 +0900

    :sparkles: Feat: SwipeableView 구현

--- On-Dot/On-Dot/View/Utils/SwipeableItemView.swift
+++ On-Dot/On-Dot/View/Utils/SwipeableItemView.swift
@@ -8,11 +8,59 @@
 import SwiftUI
 
 struct SwipeableItemView: View {
+    let item: ScheduleModel
+    var onClickToggle: (Bool) -> Void
+    var onDelete: () -> Void
+
+    @State private var offsetX: CGFloat = 0
+    private let swipeThreshold: CGFloat = -60
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .trailing) {
+            HStack {
+                Spacer()
+                Button(action: {
+                    withAnimation {
+                        offsetX = 0
+                    }
+                    onDelete()
+                }) {
+                    VStack(spacing: 7) {
+                        Image("ic_trash")
+                            .resizable()
+                            .renderingMode(.template)
+                            .frame(width: 25, height: 24)
+                            .foregroundStyle(Color.gray0)
+                        
+                        Text("삭제")
+                            .font(OnDotTypo.bodyMediumR)
+                            .foregroundColor(.gray0)
+                    }
+                }
+                .padding(.trailing, 20)
+            }
+
+            ScheduleAlarmListItemView(item: item, onClickToggle: onClickToggle)
+                .offset(x: offsetX)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            if value.translation.width < 0 {
+                                offsetX = value.translation.width
+                            }
+                        }
+                        .onEnded { value in
+                            withAnimation {
+                                if value.translation.width < swipeThreshold {
+                                    offsetX = swipeThreshold
+                                } else {
+                                    offsetX = 0
+                                }
+                            }
+                        }
+                )
+        }
     }
 }
 
-#Preview {
-    SwipeableItemView()
-}
+
