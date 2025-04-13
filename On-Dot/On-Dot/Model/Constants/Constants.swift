6dfa2b588c5e52e37580bcd62b1ac5f4d4de0bd4
dogmania <hj296914@gmail.com>
2025-04-14 04:42:52 +0900

    :sparkles: Feat: 반복 타입 enum 추가

--- On-Dot/On-Dot/Model/Constants/Constants.swift
+++ On-Dot/On-Dot/Model/Constants/Constants.swift
@@ -9,3 +9,13 @@ enum AppConstants {
     static let weekdaySymbolsKR = ["일", "월", "화", "수", "목", "금", "토"]
     static let repeatTypeTitles = ["매일", "평일", "주말"]
 }
+
+enum RepeatType: Int, CaseIterable {
+    case daily = 0
+    case weekly = 1
+    case monthly = 2
+    
+    var title: String {
+        return AppConstants.repeatTypeTitles[self.rawValue]
+    }
+}
