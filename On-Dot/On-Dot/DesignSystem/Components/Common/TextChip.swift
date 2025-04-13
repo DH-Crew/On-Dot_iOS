467083e3a715099959530207b9f3bb13837b44d6
dogmania <hj296914@gmail.com>
2025-04-14 00:31:23 +0900

    :recycle: Refactor: TextChip Style 추가

--- On-Dot/On-Dot/DesignSystem/Components/Common/TextChip.swift
+++ On-Dot/On-Dot/DesignSystem/Components/Common/TextChip.swift
@@ -9,15 +9,31 @@ import SwiftUI
 
 struct TextChip: View {
     var title: String = ""
-    var isActive: Bool = false
+    var style: Style = .normal
+    
+    enum Style {
+        case active
+        case inactive
+        case normal
+    }
     
     var body: some View {
         Text(title)
             .font(OnDotTypo.bodyMediumR)
-            .foregroundColor(isActive ? Color.green600 : Color.gray50)
+            .foregroundColor(style.foregroundColor)
             .padding(.horizontal, 8)
             .padding(.vertical, 4)
             .background(Color.gray500)
             .cornerRadius(6)
+    }
+}
+
+private extension TextChip.Style {
+    var foregroundColor: Color {
+        switch self {
+        case .active: return Color.green500
+        case .inactive: return Color.gray400
+        case .normal: return Color.gray50
+        }
     }
 }
