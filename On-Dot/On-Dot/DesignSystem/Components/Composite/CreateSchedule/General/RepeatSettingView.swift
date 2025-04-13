075106ab2b139e8eeb7c66a14818d21a39aff4b2
dogmania <hj296914@gmail.com>
2025-04-14 00:23:02 +0900

    :recycle: Refactor: 클릭 콜백 함수 추가

---
+++ On-Dot/On-Dot/DesignSystem/Components/Composite/CreateSchedule/General/RepeatSettingView.swift
@@ -0,0 +1,18 @@
+//
+//  RepeatSettingView.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 4/14/25.
+//
+
+import SwiftUI
+
+struct RepeatSettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RepeatSettingView()
+}
