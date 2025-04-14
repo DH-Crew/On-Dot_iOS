530644ca438a70e43245e53f605e78307d623105
dogmania <hj296914@gmail.com>
2025-04-14 18:20:24 +0900

    :sparkles: Feat: TopBar 구현

---
+++ On-Dot/On-Dot/DesignSystem/Components/Common/TopBar.swift
@@ -0,0 +1,27 @@
+//
+//  TopBar.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 4/14/25.
+//
+
+import SwiftUI
+
+struct TopBar: View {
+    var title: String = ""
+    let image: String
+    
+    var body: some View {
+        ZStack(alignment: .center) {
+            HStack {
+                Image(image)
+                Spacer()
+            }
+            .frame(maxWidth: .infinity)
+            
+            Text(title)
+                .font(OnDotTypo.titleSmallM)
+                .foregroundStyle(Color.gray0)
+        }
+    }
+}
