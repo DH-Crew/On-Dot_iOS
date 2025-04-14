09b3e97f111f8311f4bff328fb5cac5314a2a96b
dogmania <hj296914@gmail.com>
2025-04-14 17:28:08 +0900

    :sparkles: Feat: RecentLocationModel 구현

---
+++ On-Dot/On-Dot/Model/Location/RecentLocationModel.swift
@@ -0,0 +1,15 @@
+//
+//  RecentLocationModel.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 4/14/25.
+//
+
+import Foundation
+
+struct RecentLocationModel: Codable {
+    let title: String
+    let longitude: Double
+    let latitude: Double
+    let date: Date
+}
