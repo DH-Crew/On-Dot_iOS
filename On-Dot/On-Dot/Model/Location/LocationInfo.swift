768665af5372aedb5f10a1674abde11dba530ff2
dogmania <hj296914@gmail.com>
2025-04-15 20:59:44 +0900

    :sparkles: Feat: 장소 정보 구조체 구현

--- On-Dot/On-Dot/Model/Location/LocationInfo.swift
+++ On-Dot/On-Dot/Model/Location/LocationInfo.swift
@@ -5,7 +5,7 @@
 //  Created by 현수 노트북 on 4/15/25.
 //
 
-struct LocationSearchResult: Codable {
+struct LocationInfo: Codable {
     let title: String
     let roadAddress: String
     let latitude: Double
