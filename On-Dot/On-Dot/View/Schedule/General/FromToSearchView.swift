5c8f38c07b11d590c1a77b67b45f1fdad9f521f4
dogmania <hj296914@gmail.com>
2025-04-15 23:49:03 +0900

    :sparkles: Feat: 출발, 도착지 검색 화면 구현

--- On-Dot/On-Dot/View/Schedule/General/FromToSearchView.swift
+++ On-Dot/On-Dot/View/Schedule/General/FromToSearchView.swift
@@ -8,11 +8,110 @@
 import SwiftUI
 
 struct FromToSearchView: View {
+    @EnvironmentObject var viewModel: GeneralScheduleCreateViewModel
+    
+    @FocusState private var focusedField: FocusField?
+    
+    let selectedDate: String
+    let selectedTime: String
+    
+    var onClickBack: () -> Void
+    var isLocationSelected: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.gray900.ignoresSafeArea().onTapGesture {
+                focusedField = nil
+            }
+            
+            VStack(alignment: .leading, spacing: 0) {
+                VStack(alignment: .leading, spacing: 0) {
+                    TopBar(image: "ic_back", onClickButton: onClickBack)
+                    
+                    Spacer().frame(height: 24)
+                    
+                    StepProgressBar(totalStep: 2, currentStep: 2)
+                    
+                    Spacer().frame(height: 34)
+                    
+                    Text("출발지와 목적지를 알려주세요")
+                        .font(OnDotTypo.titleMediumM)
+                        .foregroundStyle(Color.gray0)
+                    
+                    Spacer().frame(height: 48)
+                    
+                    SelectedDateTimeView(
+                        selectedDate: selectedDate,
+                        selectedTime: selectedTime
+                    )
+                    
+                    Spacer().frame(height: 20)
+                    
+                    FromToLocationView(
+                        fromLocation: $viewModel.fromLocation,
+                        toLocation: $viewModel.toLocation,
+                        lastFocusedField: $viewModel.lastFocuesdField,
+                        focusedField: $focusedField,
+                        onValueChanged: { newValue in
+                            viewModel.currentKeyword = newValue
+                            Task {
+                                await viewModel.onValueChanged(newValue: newValue)
+                            }
+                        },
+                        onClickClose: { field in
+                            viewModel.onClickClose(field: field)
+                        }
+                    )
+                    
+                    Spacer().frame(height: 24)
+                }
+                .padding(.horizontal, 22)
+                
+                Rectangle().fill(Color.gray800).frame(maxWidth: .infinity).frame(height: 8)
+                
+                Spacer().frame(height: 20)
+                
+                ScrollView {
+                    LazyVStack(alignment: .leading, spacing: 16) {
+                        ForEach(viewModel.searchResult) { location in
+                            LocationSearchItemView(keyword: viewModel.currentKeyword, title: location.title, detail: location.roadAddress)
+                                .onTapGesture {
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                                        viewModel.onClickLocationItem(location: location)
+                                    }
+                                }
+                            Rectangle().fill(Color.gray800).frame(maxWidth: .infinity).frame(height: 0.5)
+                        }
+                    }
+                    .padding(.horizontal, 22)
+                }
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    focusedField = nil
+                }
+                .gesture(
+                    DragGesture()
+                        .onChanged { _ in
+                            focusedField = nil
+                        }
+                )
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .onAppear {
+            focusedField = .from
+        }
+        .onChange(of: viewModel.isFromLocationSelected) { _ in
+            checkIfLocationSelectionCompleted()
+        }
+        .onChange(of: viewModel.isToLocationSelected) { _ in
+            checkIfLocationSelectionCompleted()
+        }
     }
-}
-
-#Preview {
-    FromToSearchView()
+    
+    private func checkIfLocationSelectionCompleted() {
+        if viewModel.isFromLocationSelected && viewModel.isToLocationSelected {
+            isLocationSelected()
+        }
+    }
 }
