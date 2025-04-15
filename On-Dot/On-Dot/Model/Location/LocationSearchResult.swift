c2247b27308133c89870a7612b2171c79585eaa4
dogmania <hj296914@gmail.com>
2025-04-15 20:38:24 +0900

    :sparkles: Feat: 장소 검색 서버 통신 구현

---
+++ On-Dot/On-Dot/Model/Location/LocationSearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  LocationSearchResultModel.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 4/15/25.
+//
+
+struct LocationSearchResult: Codable {
+    let title: String
+    let roadAddress: String
+    let latitude: Double
+    let longitude: Double
+}
