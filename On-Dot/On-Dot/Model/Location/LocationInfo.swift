d927f4cc41bbd1f23f60af822b5eb08c9fa7476b
dogmania <hj296914@gmail.com>
2025-04-15 23:47:17 +0900

    :recycle: Refactor: Identifier 프로토콜 추가

--- On-Dot/On-Dot/Model/Location/LocationInfo.swift
+++ On-Dot/On-Dot/Model/Location/LocationInfo.swift
@@ -5,9 +5,11 @@
 //  Created by 현수 노트북 on 4/15/25.
 //
 
-struct LocationInfo: Codable {
+struct LocationInfo: Codable, Identifiable {
     let title: String
     let roadAddress: String
     let latitude: Double
     let longitude: Double
+    
+    var id: String { title + roadAddress }
 }
