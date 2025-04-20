81892044eaf399c5b804130272daa64b7d57c848
dogmania <hj296914@gmail.com>
2025-04-21 04:26:42 +0900

    :sparkles: Feat: 온보딩 1단계 구현

---
+++ On-Dot/On-Dot/View/Onboarding/OnboardingViewModel.swift
@@ -0,0 +1,18 @@
+
+import SwiftUI
+
+final class OnboardingViewModel: ObservableObject {
+    @Published var currentStep = 1
+    @Published var hourText: String = ""
+    @Published var minuteText: String = ""
+    
+    let totalStep = 5
+    
+    func onClickButton() {
+        if currentStep < 5 {
+            currentStep += 1
+        } else if currentStep == 5 {
+            // TODO: 온보딩 완료 메서드 호출
+        }
+    }
+}
---
+++ On-Dot/On-Dot/View/Onboarding/Step/OnboardingStep1View.swift
@@ -0,0 +1,82 @@
+//
+//  OnboardingStep1View.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 4/21/25.
+//
+
+import SwiftUI
+
+struct OnboardingStep1View: View {
+    @Binding var hourText: String
+    @Binding var minuteText: String
+    @FocusState.Binding var focusField: TimeFocusField?
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            (
+                Text("평소 ").foregroundColor(Color.gray0).font(OnDotTypo.titleMediumL) +
+                Text("외출 준비").foregroundColor(Color.green500).font(OnDotTypo.titleMediumL) +
+                Text("하는데\n얼마나 소요되나요?").foregroundColor(Color.gray0).font(OnDotTypo.titleMediumL)
+            )
+            .multilineTextAlignment(.leading)
+            
+            Spacer().frame(height: 16)
+            
+            Text("30분, 1시간 20분 등 자유롭게 적어주세요!")
+                .font(OnDotTypo.bodyMediumR)
+                .foregroundStyle(Color.green300)
+            
+            Spacer().frame(height: 40)
+            
+            HStack(spacing: 11) {
+                TimeInputView(
+                    content: $hourText,
+                    subText: "시간"
+                )
+                
+                TimeInputView(
+                    content: $minuteText,
+                    subText: "분"
+                )
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func TimeInputView(
+        content: Binding<String>,
+        subText: String
+    ) -> some View {
+        HStack(alignment: .center, spacing: 8) {
+            ZStack(alignment: .leading) {
+                if content.wrappedValue.isEmpty {
+                    Text("5자 이내의 숫자")
+                        .font(OnDotTypo.bodyLargeR1)
+                        .foregroundStyle(Color.gray300)
+                }
+                
+                TextField("", text: content)
+                    .onChange(of: content.wrappedValue) { newValue in
+                        if newValue.count > 5 {
+                            content.wrappedValue = String(content.wrappedValue.prefix(5))
+                        }
+                    }
+                    .keyboardType(.numberPad)
+                    .frame(maxWidth: .infinity)
+                    .font(OnDotTypo.bodyLargeR1)
+                    .foregroundStyle(Color.gray0)
+                    .focused($focusField, equals: subText == "시간" ? .hour : .minute)
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.leading, 20)
+            .padding(.vertical, 16)
+            .background(Color.gray700)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            
+            Text(subText)
+                .font(OnDotTypo.bodyLargeR1)
+                .foregroundStyle(Color.gray200)
+        }
+    }
+}
