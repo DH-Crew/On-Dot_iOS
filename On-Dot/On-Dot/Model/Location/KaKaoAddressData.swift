23522951329c4f8d4fb5d2a222c781d8be89c770
dogmania <hj296914@gmail.com>
2025-04-22 01:51:10 +0900

    :sparkles: Feat: 도로명 주소 검색 결과를 저장하는 구조체 구현

---
+++ On-Dot/On-Dot/Model/Location/KaKaoAddressData.swift
@@ -0,0 +1,12 @@
+//
+//  KaKaoAddressData.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 4/22/25.
+//
+
+struct KaKaoAddressData: Codable {
+    let roadAddress: String
+    let jibunAddress: String
+    let zonecode: String
+}
