c91419a0b8b4685e02e74b7a89777bbf9f643231
dogmania <hj296914@gmail.com>
2025-04-26 02:34:33 +0900

    :sparkles: Feat: 알람 카테고리 구현

---
+++ On-Dot/On-Dot/Model/Enum/Alarm/AlarmCategory.swift
@@ -0,0 +1,19 @@
+//
+//  AlarmCategory.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 4/23/25.
+//
+
+enum AlarmCategory: String, CaseIterable, Identifiable {
+    case category1, category2
+
+    var id: String { rawValue }
+
+    var displayName: String {
+        switch self {
+        case .category1: return "밝은 에너지"
+        case .category2: return "빠르고 강렬한"
+        }
+    }
+}
