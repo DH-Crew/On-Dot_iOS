5ade9fff77617e9eddba0e5c96082728bdaee88d
dogmania <hj296914@gmail.com>
2025-04-26 02:03:27 +0900

    :sparkles: Feat: 스와이프로 뒤로가기 유틸 로직 구현

--- On-Dot/On-Dot/View/Utils/SwipeBackEnabler.swift
+++ On-Dot/On-Dot/View/Utils/SwipeBackEnabler.swift
@@ -6,13 +6,29 @@
 //
 
 import SwiftUI
+import UIKit
 
-struct SwipeBackEnabler: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct SwipeBackEnabler: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        let vc = UIViewController()
+        DispatchQueue.main.async {
+            vc.navigationController?.interactivePopGestureRecognizer?.delegate = context.coordinator
+            vc.navigationController?.interactivePopGestureRecognizer?.isEnabled = true
+        }
+        return vc
+    }
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+    func makeCoordinator() -> Coordinator { Coordinator() }
+    
+    class Coordinator: NSObject, UIGestureRecognizerDelegate {
+        func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+            return true
+        }
     }
 }
 
-#Preview {
-    SwipeBackEnabler()
+extension View {
+  func enableSwipeBack() -> some View {
+    background(SwipeBackEnabler())
+  }
 }
