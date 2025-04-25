b800ab68a48463a2117df07aaa45b8d34143e4a9
dogmania <hj296914@gmail.com>
2025-04-26 01:05:15 +0900

    :sparkles: Feat: 새로운 파일 생성

---
+++ On-Dot/On-Dot/View/Alarm/SoundSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SoundSettingsView.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 4/23/25.
+//
+
+import SwiftUI
+
+struct SoundSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SoundSettingsView()
+}
