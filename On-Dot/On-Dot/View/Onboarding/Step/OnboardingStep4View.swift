178e5e420812cf6f3c10ab9d68cb24e444593c76
dogmania <hj296914@gmail.com>
2025-04-26 15:10:57 +0900

    :sparkles: Feat: 새로운 파일 생성

---
+++ On-Dot/On-Dot/View/Onboarding/Step/OnboardingStep4View.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingStep4View.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 4/26/25.
+//
+
+import SwiftUI
+
+struct OnboardingStep4View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingStep4View()
+}
