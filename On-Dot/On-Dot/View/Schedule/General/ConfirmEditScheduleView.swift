ce6022a2e50ff05e960051912269137cd28c0c8a
dogmania <hj296914@gmail.com>
2025-04-28 04:02:39 +0900

    :sparkles: Feat: 파일 생성

---
+++ On-Dot/On-Dot/View/Schedule/General/ConfirmEditScheduleView.swift
@@ -0,0 +1,21 @@
+//
+//  ConfirmScheduleView.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 4/26/25.
+//
+
+import SwiftUI
+
+struct ConfirmScheduleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+private struct ConfirmScheduleTopBar: View {
+    
+    var body: some View {
+        
+    }
+}
