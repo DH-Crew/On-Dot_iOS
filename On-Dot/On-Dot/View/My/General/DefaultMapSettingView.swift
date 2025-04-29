d334a6f595be6defe1e6f1bd06f0c21cc9e43f51
dogmania <hj296914@gmail.com>
2025-04-30 04:23:58 +0900

    :sparkles: Feat: DefaultMapSettingView 구현

--- On-Dot/On-Dot/View/My/General/DefaultMapSettingView.swift
+++ On-Dot/On-Dot/View/My/General/DefaultMapSettingView.swift
@@ -8,11 +8,72 @@
 import SwiftUI
 
 struct DefaultMapSettingView: View {
+    @EnvironmentObject private var viewModel: MyPageViewModel
+    
+    var onClickBackButton: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.black.ignoresSafeArea()
+            Color.gray900.ignoresSafeArea()
+            
+            VStack {
+                TopBar(
+                    title: "길 안내 지도 설정",
+                    image: "ic_back",
+                    onClickButton: onClickBackButton
+                )
+                
+                Spacer().frame(height: 32)
+                
+                Text("자주 사용하는\n지도를 설정해주세요")
+                    .font(OnDotTypo.titleMediumM)
+                    .foregroundStyle(Color.gray0)
+                    .multilineTextAlignment(.leading)
+                
+                Spacer().frame(height: 16)
+                
+                Text("길 안내에 사용될 예정이에요.")
+                    .font(OnDotTypo.bodyMediumR)
+                    .foregroundStyle(Color.green300)
+                
+                Spacer().frame(height: 40)
+                
+                HStack(spacing: 16) {
+                    mapItem(type: .naver)
+                    mapItem(type: .kakao)
+                }
+                .frame(maxWidth: .infinity)
+                
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding(.horizontal, 22)
+        }
     }
-}
-
-#Preview {
-    DefaultMapSettingView()
+    
+    @ViewBuilder
+    private func mapItem(
+        type: MapProvider.MapType
+    ) -> some View {
+        VStack(alignment: .center, spacing: 16) {
+            Image(type.image)
+            
+            Text(type.title)
+                .font(OnDotTypo.bodyMediumR)
+                .foregroundStyle(Color.gray0)
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.vertical, 24)
+        .background(Color.gray700)
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .overlay(
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(viewModel.selectedMapType == type ? Color.green600 : Color.clear, lineWidth: 1)
+        )
+        .contentShape(Rectangle())
+        .onTapGesture {
+            viewModel.selectedMapType = type
+        }
+    }
 }
