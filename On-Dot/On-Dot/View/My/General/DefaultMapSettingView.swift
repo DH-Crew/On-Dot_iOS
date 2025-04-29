5d99237cef2c6b75513950b9f21cbbf23f177b2f
dogmania <hj296914@gmail.com>
2025-04-30 04:23:21 +0900

    :sparkles: Feat: 새 파일 생성

---
+++ On-Dot/On-Dot/View/My/General/DefaultMapSettingView.swift
@@ -0,0 +1,18 @@
+//
+//  DefaultMapSettingView.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 4/30/25.
+//
+
+import SwiftUI
+
+struct DefaultMapSettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DefaultMapSettingView()
+}
