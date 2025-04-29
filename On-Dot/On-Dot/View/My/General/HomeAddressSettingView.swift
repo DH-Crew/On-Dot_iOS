5b537cc0e9beeef9a6d87fd3a1ca9cb3b9a11349
dogmania <hj296914@gmail.com>
2025-04-30 00:46:37 +0900

    :sparkles: Feat: HomeAddressSettingView 구현

--- On-Dot/On-Dot/View/My/General/HomeAddressSettingView.swift
+++ On-Dot/On-Dot/View/My/General/HomeAddressSettingView.swift
@@ -8,11 +8,67 @@
 import SwiftUI
 
 struct HomeAddressSettingView: View {
+    @EnvironmentObject private var viewModel: MyPageViewModel
+    
+    var onClickBackButton: () -> Void
+    var onClickEditButton: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.black.ignoresSafeArea()
+            Color.gray900.ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                TopBar(
+                    title: "집 주소 설정",
+                    image: "ic_back",
+                    onClickButton: onClickBackButton
+                )
+                
+                Spacer().frame(height: 32)
+                
+                HStack(alignment: .center, spacing: 0) {
+                    Image("ic_home_address")
+                    
+                    Spacer().frame(width: 16)
+                    
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text("집")
+                            .font(OnDotTypo.bodyMediumM)
+                            .foregroundStyle(Color.gray200)
+                        
+                        Spacer().frame(height: 4)
+                        
+                        Text(viewModel.homeAddress.roadAddress)
+                            .font(OnDotTypo.bodyLargeR1)
+                            .foregroundStyle(Color.gray0)
+                        
+                        Spacer().frame(height: 4)
+                        
+                        Text(viewModel.homeAddress.roadAddress)
+                            .font(OnDotTypo.bodyMediumR)
+                            .foregroundStyle(Color.gray300)
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: onClickEditButton) {
+                        Image("ic_pencil")
+                            .resizable()
+                            .renderingMode(.template)
+                            .foregroundStyle(Color.gray400)
+                            .frame(width: 20, height: 20)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 16)
+                .padding(.horizontal, 20)
+                .background(Color.gray700)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+                
+                Spacer()
+            }
+            .padding(.horizontal, 22)
+        }
     }
-}
-
-#Preview {
-    HomeAddressSettingView()
 }
