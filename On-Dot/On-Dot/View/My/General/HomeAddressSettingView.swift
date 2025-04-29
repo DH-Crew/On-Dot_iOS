b26a45296c1f014cc9f8e670f772abe72b1e8040
dogmania <hj296914@gmail.com>
2025-04-30 00:45:50 +0900

    :sparkles: Feat: 새 파일 생성

---
+++ On-Dot/On-Dot/View/My/General/HomeAddressSettingView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeAddressSettingView.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 4/30/25.
+//
+
+import SwiftUI
+
+struct HomeAddressSettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeAddressSettingView()
+}
