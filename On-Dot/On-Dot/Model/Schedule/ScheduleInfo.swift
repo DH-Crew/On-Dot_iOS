c5573802ed7f9213e9c9a895e3e33dfb7fb957df
dogmania <hj296914@gmail.com>
2025-05-01 01:41:31 +0900

    :recycle: Refactor: 프로퍼티 선언 수정 및 파일명 수정

--- On-Dot/On-Dot/Model/Schedule/ScheduleInfo.swift
+++ On-Dot/On-Dot/Model/Schedule/ScheduleInfo.swift
@@ -6,12 +6,35 @@
 //
 
 struct ScheduleInfo: Codable {
-    let title: String
-    let isRepeat: Bool
-    let repeatDays: [Int]
-    let appointmentAt: String
-    let departurePlace: LocationInfo
-    let arrivalPlace: LocationInfo
-    let preparationAlarm: AlarmInfo
-    let departureAlarm: AlarmInfo
+    var title: String
+    var isRepeat: Bool
+    var repeatDays: [Int]
+    var appointmentAt: String
+    var departurePlace: LocationInfo
+    var arrivalPlace: LocationInfo
+    var preparationAlarm: AlarmInfo
+    var departureAlarm: AlarmInfo
+}
+
+extension ScheduleInfo {
+    static let placeholder = ScheduleInfo(
+        title: "스터디 모임",
+        isRepeat: true,
+        repeatDays: [2, 4, 6],
+        appointmentAt: "2025-05-10T19:00:00",
+        departurePlace: LocationInfo(
+            title: "집",
+            roadAddress: "서울특별시 강남구 테헤란로 123",
+            latitude: 37.4979,
+            longitude: 127.0276
+        ),
+        arrivalPlace: LocationInfo(
+            title: "카페",
+            roadAddress: "서울특별시 서초구 서초대로 77",
+            latitude: 37.501,
+            longitude: 127.029
+        ),
+        preparationAlarm: .placeholder,
+        departureAlarm: .placeholder
+    )
 }
