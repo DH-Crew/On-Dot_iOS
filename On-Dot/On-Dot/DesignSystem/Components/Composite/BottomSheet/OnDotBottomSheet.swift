3abfd8874b5f650a263642fbf1b3e8766f2d8dd7
dogmania <hj296914@gmail.com>
2025-05-03 18:45:01 +0900

    :sparkles: Feat: OnDotBottomSheet 구현

---
+++ On-Dot/On-Dot/DesignSystem/Components/Composite/BottomSheet/OnDotBottomSheet.swift
@@ -0,0 +1,53 @@
+//
+//  OnDotBottomSheet.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 5/3/25.
+//
+
+import SwiftUI
+
+struct OnDotBottomSheet<Content: View>: View {
+    var onDismissRequest: () -> Void
+    
+    let content: Content
+    
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.4)
+                .ignoresSafeArea()
+                .onTapGesture { onDismissRequest() }
+            
+            VStack {
+                Spacer()
+                
+                content
+                    .frame(maxWidth: .infinity)
+                    .padding(.horizontal, 22)
+                    .padding(.top, 32)
+                    .padding(.bottom, 16)
+                    .background(
+                        Color.gray700
+                            .clipShape(
+                                RoundedCorner(radius: 20, corners: [.topLeft, .topRight])
+                            )
+                    )
+            }
+        }
+    }
+}
+
+private On-Dot/On-Dot/DesignSystem/Components/Composite/BottomSheet/OnDotBottomSheet.swift
+struct RoundedCorner: Shape {
+    var radius: CGFloat
+    var corners: UIRectCorner
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius)
+        )
+        return Path(path.cgPath)
+    }
+}
