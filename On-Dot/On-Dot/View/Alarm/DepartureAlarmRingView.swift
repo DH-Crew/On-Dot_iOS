d2da20a84252197858b95b2575ad7eca82d9bb36
dogmania <hj296914@gmail.com>
2025-05-05 18:48:38 +0900

    :sparkles: Feat: 새로운 파일 생성

---
+++ On-Dot/On-Dot/View/Alarm/DepartureAlarmRingView.swift
@@ -0,0 +1,18 @@
+//
+//  DepartureAlarmRingView.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 5/5/25.
+//
+
+import SwiftUI
+
+struct DepartureAlarmRingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DepartureAlarmRingView()
+}
