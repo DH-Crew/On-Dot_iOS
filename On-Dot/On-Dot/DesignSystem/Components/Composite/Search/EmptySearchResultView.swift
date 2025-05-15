9aca063934467a8b9f533c1b87bf2149989c006f
dogmania <hj296914@gmail.com>
2025-05-15 13:11:28 +0900

    :sparkles: Feat: 검색 결과가 없을 때 뷰 구현

---
+++ On-Dot/On-Dot/DesignSystem/Components/Composite/Search/EmptySearchResultView.swift
@@ -0,0 +1,30 @@
+//
+//  EmptySearchResultView.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 5/15/25.
+//
+
+import SwiftUI
+
+struct EmptySearchResultView: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 0) {
+            Spacer().frame(height: 80)
+            
+            Image("ic_caution")
+                .resizable()
+                .renderingMode(.template)
+                .foregroundStyle(Color.gray500)
+                .frame(width: 63, height: 63)
+            
+            Spacer().frame(height: 23)
+            
+            Text("검색결과를 찾을 수 없습니다.\n검색어를 확인해주세요.")
+                .font(OnDotTypo.titleSmallM)
+                .foregroundStyle(Color.gray500)
+                .multilineTextAlignment(.center)
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
