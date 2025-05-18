ee285f3a973d2c5f9db1f9cbb8965de833869b38
dogmania <hj296914@gmail.com>
2025-05-18 15:05:50 +0900

    :recycle: Refactor: SingleTextField 공통 뷰 구현

---
+++ On-Dot/On-Dot/DesignSystem/Components/Composite/TextField/SingleTextFieldView.swift
@@ -0,0 +1,56 @@
+//
+//  SingleTextFieldView.swift
+//  On-Dot
+//
+//  Created by 현수 노트북 on 5/18/25.
+//
+
+import SwiftUI
+
+struct SingleTextFieldView: View {
+    @FocusState.Binding var focusState: Bool
+    @Binding var input: String
+    
+    var onValueChanged: (String) -> Void
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            HStack(spacing: 4) {
+                TextField(
+                    "",
+                    text: $input
+                )
+                .frame(maxWidth: .infinity)
+                .focused($focusState)
+                .font(OnDotTypo.bodyLargeR1)
+                .foregroundStyle(Color.gray0)
+                .onChange(of: input) { newValue in
+                    onValueChanged(newValue)
+                }
+                
+                if !input.isEmpty {
+                    Image("ic_close")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundStyle(Color.gray400)
+                        .frame(width: 16, height: 16)
+                        .onTapGesture {
+                            input = ""
+                        }
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.horizontal, 20)
+            .padding(.vertical, 16)
+            .background(Color.gray700)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            
+            if input.isEmpty {
+                Text("입력")
+                    .font(OnDotTypo.bodyLargeR1)
+                    .foregroundStyle(Color.gray300)
+                    .padding(.horizontal, 20)
+            }
+        }
+    }
+}
